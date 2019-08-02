ea79b498e4f5ac9c337098ecf3056df1d89cdd79
Yunjae Hwang <yunjaeh@stanford.edu>
2019-08-01 18:14:36 -0700

    print function complete

--- ACH/ACH.py
+++ ACH/ACH.py
@@ -1,102 +1,128 @@
+import os
 import numpy as np
+import math
 import matplotlib.pyplot as plt
-
 from datetime import datetime
 
 
-class ACH():
-    def __init__(self):
-        self.fname = ''
+class postProcess():
+    def __init__(self,fname,dirData='./',dirFig='./'):
+        self.fname = fname
+        self.dirData = dirData
+        self.dirFig = dirFig
         self.time = []
-        self.c_raw = []
+        self.cRaw = []
         self.c = np.empty(0)
-        self.ACH = []
-        
-        self.time_avg = []
-        self.c_avg = []
-        self.ACH_avg = []
-        
-        self.num_test = 0
-        self.data_idx = []
+        self.ACH = {}
+        self.ACH_target = {}
+        self.range = range(600, 1200)
+        self.numTest = 0
+        self.dataIdx = []
+        self.cRawMinMax = []
 
-    def read_input(self,fname):
-        self.fname=fname
-        with open(fname) as fp:
+        if (dirFig != './'):
+            os.system('mkdir '+dirFig)
+        
+        with open(self.dirData + self.fname) as fp:
             idx_temp = 0
             for line in fp:
                 line_split = line.split(',')
                 # print(line_split)
                 try :
                     if(line_split[0]=='MM/dd/yyyy'):
-                        self.num_test+=1
-                        self.data_idx.append(idx_temp)
+                        self.numTest+=1
+                        self.dataIdx.append(idx_temp)
                         self.ACH.append([])
                     ts = datetime.strptime(' '.join(line_split[0:2]),'%m/%d/%Y %H:%M:%S')
                     self.time.append(ts)
-                    self.c_raw.append(float(line_split[2].replace('\n','')))
+                    self.cRaw.append(float(line_split[2].replace('\n','')))
                     idx_temp += 1
                 except:
                     continue
-            self.data_idx.append(idx_temp+1)
+            self.dataIdx.append(idx_temp+1)
 #             print(self.ACH)
             span_smooth = 61
-            self.c = np.convolve(np.log(self.c_raw), np.ones((span_smooth,))/span_smooth, mode='same')
-                    
-    def print_data_summary(self):
+            self.c = np.convolve(np.log(self.cRaw), np.ones((span_smooth,))/span_smooth, mode='same')
+        
+        for i in range(self.numTest):
+            self.cRawMinMax.append( (min(self.cRaw[self.dataIdx[i]:self.dataIdx[i+1]]), \
+                max(self.cRaw[self.dataIdx[i]:self.dataIdx[i+1]])))
+
+    def __repr__(self):
         # print summary of raw data
         # the number of experiments in the file
         # minimum and maximum concentration of  each experiment
-        print('# of test:', self.num_test)
-        print('idx data:', self.data_idx)
-        for i in range(self.num_test):
-            print('  Test #: ',i+1)
-            print('Min PM:', min(self.c_raw[self.data_idx[i]:self.data_idx[i+1]]))
-            print('Max PM:', max(self.c_raw[self.data_idx[i]:self.data_idx[i+1]]))
-            
-    def plot_data(self):
-        plt.plot(range(len(self.c)),np.log(self.c_raw))
+        message = []
+        message.append('# of test:'+str(self.numTest))
+        # print('idx data:', self.dataIdx)
+        for i in range(self.numTest):
+            message.append(' Test '+str(i+1) )
+            message.append(' * PM \t (min, max):'+str(self.cRawMinMax[i]))
+            if (i+1) in self.ACH_target.keys():
+                tempACH = self.ACH_target[i+1]
+                message.append(' * ACH \t (min, max): ' \
+                    + str(round(tempACH.min(),2))+', ' + str(round(tempACH.max(),2)) )
+                message.append(' \t Mean: '+str(round(tempACH.mean(),2)) + \
+                ',\t Std:  '+str(round(tempACH.std(),2)) )
+ 
+            message.append('')
+
+        
+        return '\n'.join(message)
+
+
+    def plotRaw(self):
+        plt.plot(range(len(self.c)),np.log(self.cRaw))
         plt.plot(range(len(self.c)),self.c)
         plt.xlabel('Time [sec]')
         plt.ylabel('log(Concentration)')
         plt.title('Raw and smoothed data')
-        plt.savefig(self.fname.replace('.csv','.png'))
+        plt.savefig(self.dirFig + self.fname.replace('.csv','.png'))
         plt.cla()
 #         plt.show()
-        
-        
-    def minute_avg(self):
-        raise Exception('Not implemented yet')
     
-    def compute(self, target_test):
-        if(target_test > self.num_test):
+    def compute(self, testIdx):
+        if(testIdx > self.numTest):
             raise Exception('Test number not valid')
         else:
-            tt = self.time[self.data_idx[target_test-1]:self.data_idx[target_test]]
-            cc = self.c[self.data_idx[target_test-1]:self.data_idx[target_test]]
+            tt = self.time[self.dataIdx[testIdx-1]:self.dataIdx[testIdx]]
+            cc = self.c[self.dataIdx[testIdx-1]:self.dataIdx[testIdx]]
         # print(len(tt),len(cc))
         id_max = np.argmax(cc)
         c_max = max(cc)
         len_data = len(cc)
         t_target = np.arange(1, len_data-id_max)
         c_target = cc[id_max+1:len_data+1]
+        self.ACH[testIdx]=(c_target - c_max)/t_target * -3600
 
-        self.ACH[target_test-1]=(c_target - c_max)/t_target * -3600
-
-    def plot_ACH(self,target_test):
-        if(target_test > self.num_test):
+    def plot(self,testIdx,markRange=False):
+        if(testIdx > self.numTest):
             raise Exception('Test number not valid')
-        
-#             print(len(self.ACH[target_test-1]))
-#             print(range(len(self.ACH[target_test-1])))
-        plt.plot(range(len(self.ACH[target_test-1])),self.ACH[target_test-1])
+        plt.plot(range(len(self.ACH[testIdx])),self.ACH[testIdx])
+        plt.plot(self.range,self.ACH[testIdx][self.range],'x')
         plt.xlabel('Time [sec]')
         plt.ylabel('ACH [1/hr]')
         # plt.show()
-        plt.savefig(self.fname.replace('.csv','')+'_'+str(target_test)+'.png')
+        plt.savefig(self.dirFig + self.fname.replace('.csv','') \
+            +'_'+str(testIdx)+'.png')
         plt.cla()
+
+    def getACH(self,testIdx,period=(600,1200)):
+        self.range = range(period[0],period[1])
+        self.ACH_target[testIdx] = self.ACH[testIdx][self.range]
+        return self.ACH_target[testIdx]
+        # [range(period[0],period[2])]
+        
+
+        
+    def minute_avg(self):
+        self.time_avg = []
+        self.c_avg = []
+        self.ACH_avg = []
+        raise Exception('Not implemented yet')
+
+    def compute_time_avg(self,numTest):
+        raise Exception('Not implemented yet')
     
-def compute_time_avg(self,num_test):
-    raise Exception('Not implemented yet')
-    
-def plot_ACH_time_avg(self,num_test):
-    raise Exception('Not implemented yet')
+    def plot_ACH_time_avg(self,numTest):
+        raise Exception('Not implemented yet')
--- ACH/main.py
+++ ACH/main.py
@@ -1,25 +1,20 @@
 import ACH
 import os 
 
-dir_raw = './'
+dir_raw = './data/'
+dir_fig = './infiltration/'
+fileListDict = {}
+
+fileCount = 0
 for f_input in os.listdir(dir_raw):
     if '.csv' in f_input:
-        print(f_input)
-        ACH_test = ACH.ACH()
-        ACH_test.read_input(dir_raw+f_input)
-        ACH_test.plot_data()
-        for i in range(2,ACH_test.num_test+1):
-            print(i)
+        fileCount += 1
+        fileListDict[fileCount] = f_input
+
+        ACH_test = ACH.postProcess(f_input,dirData=dir_raw,dirFig=dir_fig)
+        ACH_test.plotRaw()
+        for i in range(2,ACH_test.numTest+1):
             ACH_test.compute(i)
-            ACH_test.plot_ACH(i)
-
-
-
-
-# file_test = 'Data_O_46-47-48_20181015.csv'
-# ACH_test = ACH_process()
-# ACH_test.read_input(file_test)
-# ACH_test.print_data_summary()
-# ACH_test.plot_raw()
-
-
+            ACH_test.plot(i)
+            ACH_range = ACH_test.getACH(i)
+        print(ACH_test)
